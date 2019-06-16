d9a79167c014c019b2be3ab0790455b71fe9b208
EugeneKhozhainov <zheka1114@gmail.com>
2019-06-16 18:04:34 +0300

    final

--- web/src/main/java/com/itacademy/controller/CarController.java
+++ web/src/main/java/com/itacademy/controller/CarController.java
@@ -1,35 +1,29 @@
 package com.itacademy.controller;
 
-import com.itacademy.config.WebConfig;
-import com.itacademy.database.entity.CarEntity;
 import com.itacademy.service.service.CarService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping
-    public String doGet(Model model) {
-        return "car";
+    @Autowired
+    private CarService carService;
+
+    @GetMapping(value = "/cars")
+    public String getReview(Model model) {
+        model.addAttribute("carList", carService.getFiltered("BMW", "X6", 200.0, 1, 5));
+        return "carList";
     }
 
     @PostMapping
-    public String doPost(Model model) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
-        CarService carService = context.getBean(CarService.class);
-        List<CarEntity> list = carService.getFiltered("BMW", "X6", 200.0, 1, 10);
-        model.addAttribute("carList", list);
-        return "car";
+    public String doPost(Model model, @ModelAttribute("carForm") CarForm carForm) {
+        System.out.println("carForm="+carForm);
+        model.addAttribute("carList", carService.getFiltered("BMW", "X6", 200.0, carForm.getPageNumber(), carForm.getPageCount()));
+        return "carList";
     }
-
 }
